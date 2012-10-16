d1ba0dff8bd05097b2b2fb97dc7edf125da02466
carmjc <carm@carmgate.net>
2012-10-16 15:23:07 +0200

    The console is now only shown in debug mode.

--- src/main/java/net/carmgate/morph/ui/renderer/UIRenderer.java
+++ src/main/java/net/carmgate/morph/ui/renderer/UIRenderer.java
@@ -23,31 +23,31 @@ public class UIRenderer {
 	}
 
 	public void render(int glMode, RenderStyle drawType) {
-		LOGGER.trace("Rendering interface");
-		TextureImpl.bindNone();
-
-		GL11.glScalef(WorldRenderer.scale, WorldRenderer.scale, WorldRenderer.scale);
-		// Drawing console box
-		GL11.glColor4f(0.7f, 0.7f, 0.9f, 1f);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(-Main.WIDTH / 2, Main.HEIGHT / 2);
-		GL11.glVertex2f(Main.WIDTH / 2, Main.HEIGHT / 2);
-		GL11.glVertex2f(Main.WIDTH / 2, Main.HEIGHT / 2 - 75);
-		GL11.glVertex2f(-Main.WIDTH / 2, Main.HEIGHT / 2 - 75);
-		GL11.glEnd();
-
-		// Writing in the console box
-		String modes = "Debug mode : " + WorldRenderer.debugDisplay;
-		font.drawString(-Main.WIDTH / 2 + 10, Main.HEIGHT / 2 - 65, modes, Color.black);
-		font.drawString(-Main.WIDTH / 2 + 10, Main.HEIGHT / 2 - 50, "Mouse position : " + MorphMouse.getX() + "x" + MorphMouse.getY(), Color.black);
-
-		// if debug mode : drawing positions in the screen
 		if (drawType == RenderStyle.DEBUG) {
+			LOGGER.trace("Rendering interface");
+			TextureImpl.bindNone();
+
+			GL11.glScalef(WorldRenderer.scale, WorldRenderer.scale, WorldRenderer.scale);
+			// Drawing console box
+			GL11.glColor4f(0.7f, 0.7f, 0.9f, 1f);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(-Main.WIDTH / 2, Main.HEIGHT / 2);
+			GL11.glVertex2f(Main.WIDTH / 2, Main.HEIGHT / 2);
+			GL11.glVertex2f(Main.WIDTH / 2, Main.HEIGHT / 2 - 75);
+			GL11.glVertex2f(-Main.WIDTH / 2, Main.HEIGHT / 2 - 75);
+			GL11.glEnd();
+
+			// Writing in the console box
+			String modes = "Debug mode : " + WorldRenderer.debugDisplay;
+			font.drawString(-Main.WIDTH / 2 + 10, Main.HEIGHT / 2 - 65, modes, Color.black);
+			font.drawString(-Main.WIDTH / 2 + 10, Main.HEIGHT / 2 - 50, "Mouse position : " + MorphMouse.getX() + "x" + MorphMouse.getY(), Color.black);
+
+			// if debug mode : drawing positions in the screen
 			font.drawString(-Main.WIDTH / 2 + 5, -Main.HEIGHT / 2 + 5, -Main.WIDTH / 2 + "x" + -Main.HEIGHT / 2, Color.white);
 			font.drawString(Main.WIDTH / 2 - 65, -Main.HEIGHT / 2 + 5, Main.WIDTH / 2 + "x" + -Main.HEIGHT / 2, Color.white);
 			font.drawString(-Main.WIDTH / 2 + 5, Main.HEIGHT / 2 - 99, -Main.WIDTH / 2 + "x" + Main.HEIGHT / 2, Color.white);
 			font.drawString(Main.WIDTH / 2 - 60, Main.HEIGHT / 2 - 99, Main.WIDTH / 2 + "x" + Main.HEIGHT / 2, Color.white);
+			GL11.glScalef(1 / WorldRenderer.scale, 1 / WorldRenderer.scale, 1 / WorldRenderer.scale);
 		}
-		GL11.glScalef(1 / WorldRenderer.scale, 1 / WorldRenderer.scale, 1 / WorldRenderer.scale);
 	}
 }
